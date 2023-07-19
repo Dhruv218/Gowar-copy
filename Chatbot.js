9c8ba0fc98ad8b3328d7235eecbd47c4f75649a2
Safa Asgar <thulk283@gmail.com>
2023-07-20 00:29:53 +0600

    Removed Comment

--- Chatbot.js
+++ Chatbot.js
@@ -1,9 +1,4 @@
-// chatbot.js
-// const wss = new WebSocket("wss://s9170.blr1.piesocket.com/v3/1?api_key=wBcoKk96vynh1Z6xI7DNGGjkrsLAKu4agE5ko9Zl&notify_self=1")
-
-// let wss = new WebSocket("ws://13.126.248.19/ws/chat/agent/49fae2353f3146558e3e9448489ab601/")
 var wss = new WebSocket("wss://test.gowarranty.in/ws/chat/49fae2353f3146558e3e9448489ab601/")
-// const wss = new WebSocket("ws://13.126.248.19/ws/chat/b6195e54416345f2b8ffbd46dca35c13/")
 
 let noCount = 0;
 var agentChat = false;
@@ -16,7 +11,6 @@ function handleNo(e) {
     wss = new WebSocket("wss://test.gowarranty.in/ws/chat/agent/49fae2353f3146558e3e9448489ab601/")
     wss.addEventListener("open", ws => {
       console.log("websocket connected");
-      // wss.send("Hello")
     })
     wss.addEventListener("message", (data) => {
       attatchBotMessage(data)
@@ -32,17 +26,6 @@ function handleNo(e) {
 
     const supportCountdown = document.createElement("div");
     supportCountdown.className = "flex flex-col mb-2 items-end";
-    //   supportCountdown.innerHTML = `
-    // <div class="flex items-end justify-center  mt-2">
-    //   <div class="p-2 bg-[#212334] text-center  rounded-lg">
-    //     <p class="text-[#B4B4B4] text-sm">Support executive will be assisting 
-    //     you in 
-    //     <span id="chatbot_timer" class="text-[#C73838]">00:15</span>
-    //     </p>
-    //   </div>
-    // </div>
-    // `;
-    //   messages.appendChild(supportCountdown);
     supportCountdown.innerHTML = `
   <div class="flex items-end justify-center  mt-2">
     <div class="p-2 bg-[#212334] text-center  rounded-lg">
@@ -60,10 +43,6 @@ function handleNo(e) {
     const duration = 5;
     let timer = duration;
     const countdownInterval = setInterval(() => {
-      // const minutes = Math.floor(timer / 60).toString().padStart(2, '0');
-      // const seconds = (timer % 60).toString().padStart(2, '0');
-      // const countdownText = `${minutes}:${seconds}`;
-      // countdownElement.textContent = countdownText;
       timer--;
 
 
@@ -103,7 +82,6 @@ wss.addEventListener("message", (data) => {
 })
 
 function attatchBotMessage(data) {
-  // console.log(data.data);
   console.log("inside message event");
   let botMessageText = JSON.parse(data.data)
   console.log(botMessageText)
@@ -113,7 +91,7 @@ function attatchBotMessage(data) {
     botMessage.innerHTML = `
           <div class="flex flex-col items-end justify-center">
             <div class="flex items-end justify-center">
-            <img src="./Assets/botImage.png" class="w-5 h-5 rounded-full mr-2 block" alt="" />
+            <img src="./Assets/botImage.png" class="w-5 h-5 rounded-full mr-2 block" alt="Bot" />
             <div class="text-left pe-9">
               <div class="p-2 bg-gray-100 w-fit text-left rounded-lg">
                 ${botMessageText.message}
@@ -128,7 +106,7 @@ function attatchBotMessage(data) {
     botMessage.innerHTML = `
   <div class="flex flex-col items-end justify-center">
     <div class="flex items-end justify-center">
-    <img src="./Assets/botImage.png" class="w-5 h-5 rounded-full mr-2 block" alt="" />
+    <img src="./Assets/botImage.png" class="w-5 h-5 rounded-full mr-2 block" alt="Bot" />
     <div class="text-left pe-9">
       <div class="p-2 bg-gray-100 w-fit text-left rounded-lg">
         ${botMessageText.message}
@@ -159,9 +137,6 @@ function sendMessage(event) {
     "message": newMessage
   }));
   console.log("send message called");
-
-  // console.log(botMessage)
-
   if (newMessage !== "") {
 
     const timestamp = new Date().toLocaleTimeString([], {
@@ -178,7 +153,7 @@ function sendMessage(event) {
     userMessage.className = "flex flex-col mb-2 items-end";
     userMessage.innerHTML = `
   <div class="flex items-end justify-center">
-    <img src="./Assets/botImage.png" class="w-5 h-5 rounded-full mr-2 hidden" alt="" />
+    <img src="./Assets/botImage.png" class="w-5 h-5 rounded-full mr-2 hidden" alt="bot" />
     <div class="p-2 bg-cyan-500 text-right text-white rounded-lg w-fit">
       ${newMessage}
     </div>
@@ -197,9 +172,6 @@ function sendMessage(event) {
 
 
     input.value = "";
-    // <div class="text-xs mt-2 text-gray-400 ml-8 text-left">
-    //   ${timestamp}
-    // </div>
     console.log(messages);
     setTimeout(scrollToBottom, 500);
   }
@@ -209,10 +181,6 @@ function scrollToBottom() {
   const scrollToBottomRef = document.getElementById("scrollToBottomRef");
   scrollToBottomRef.scrollIntoView({ behavior: "smooth", block: "end" });
 }
-// function scrollToBottom() {
-//   const messages = document.getElementById("chatMessages");
-//   messages.scrollTop = messages.scrollHeight;
-// }
 
 function toggleChatbot() {
 
