b474823db97981e6cb8bc4b6aa18612793497fd3
Safa Asgar <70315479+SaFaUU@users.noreply.github.com>
2023-08-03 22:00:08 +0600

    Added Typing animation

--- Chatbot.js
+++ Chatbot.js
@@ -1,20 +1,23 @@
-console.log(keywords)
-var wss = new WebSocket("wss://test.gowarranty.in/ws/chat/49fae2353f3146558e3e9448489ab601/")
+var wss = new WebSocket(
+  "wss://test.gowarranty.in/ws/chat/49fae2353f3146558e3e9448489ab601/"
+);
 
 let noCount = 0;
 var agentChat = false;
+var generatingMessage = true;
 
-function handleNo(e) {
-
+function handleYes(e) {
   if (agentChat) {
     wss.close();
-    wss = new WebSocket("wss://test.gowarranty.in/ws/chat/agent/49fae2353f3146558e3e9448489ab601/")
-    wss.addEventListener("open", ws => {
-      console.log("websocket connected");
-    })
+    wss = new WebSocket(
+      "wss://test.gowarranty.in/ws/chat/agent/49fae2353f3146558e3e9448489ab601/"
+    );
+    wss.addEventListener("open", (ws) => {
+      console.log("Agent websocket connected");
+    });
     wss.addEventListener("message", (data) => {
-      attatchBotMessage(data)
-    })
+      attatchBotMessage(data);
+    });
 
     const helpfulElements = document.getElementsByClassName("chatbot-helpful");
 
@@ -39,15 +42,13 @@ function handleNo(e) {
   `;
     messages.appendChild(supportCountdown);
 
-    const countdownElement = document.querySelector('#chatbot_timer');
+    const countdownElement = document.querySelector("#chatbot_timer");
     const duration = 5;
     let timer = duration;
     const countdownInterval = setInterval(() => {
       timer--;
 
-
       if (timer < 0) {
-
         clearInterval(countdownInterval);
 
         countdownElement.parentNode.parentNode.parentNode.parentNode.remove();
@@ -62,7 +63,6 @@ function handleNo(e) {
           </div>
           `;
         messages.appendChild(customerSupport);
-
       }
     }, 100);
   }
@@ -70,29 +70,59 @@ function handleNo(e) {
 
 function checkMessage(userInput) {
   const lowerUserInput = userInput.toLowerCase();
-  const foundKeyword = keywords.find(keyword =>
+  const foundKeyword = keywords.find((keyword) =>
     lowerUserInput.includes(keyword.toLowerCase())
   );
 
   return !!foundKeyword;
 }
 
-
-wss.addEventListener("open", ws => {
+wss.addEventListener("open", (ws) => {
   console.log("websocket connected");
-})
-
+});
 
 const messages = document.getElementById("chatMessages");
 
-// wss.addEventListener("message", (data) => {
-//   attatchBotMessage(data)
-// })
+wss.addEventListener("message", (data) => {
+  attatchBotMessage(data);
+});
+
+function addTyping() {
+  const messages = document.getElementById("chatMessages");
+  const typingAnimation = document.createElement("div");
+  typingAnimation.setAttribute("id", "typing");
+  typingAnimation.className = "flex flex-col mb-2 items-start";
+  typingAnimation.innerHTML = `
+  <div class="flex flex-col items-end justify-center">
+    <div class="flex items-end justify-center">
+      <img src="./Assets/botImage.png" class="w-5 h-5 rounded-full mr-2 block" alt="Bot" />
+      <div class="text-left pe-9">
+        <div class="p-2 bg-gray-100 w-fit text-left rounded-lg  rounded-bl-none">
+          <div class="flex">
+            <div class="w-[10px] h-[10px] bg-[#D9D9D9] rounded-full mr-1 animate-wave-1"></div>
+            <div class="w-[10px] h-[10px] bg-[#D9D9D9] rounded-full mr-1 animate-wave-2"></div>
+            <div class="w-[10px] h-[10px] bg-[#D9D9D9] rounded-full animate-wave-3"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
+  messages.appendChild(typingAnimation);
+}
+
+function removeTyping() {
+  const typingDiv = document.getElementById("typing");
+  if (typingDiv && typingDiv.parentNode) {
+    typingDiv.parentNode.removeChild(typingDiv);
+  }
+}
 
 function attatchBotMessage(data) {
   console.log("inside message event");
-  let botMessageText = JSON.parse(data.data)
-  console.log(botMessageText)
+  removeTyping();
+  let botMessageText = JSON.parse(data.data);
+  console.log(botMessageText);
   const botMessage = document.createElement("div");
   botMessage.className = "flex flex-col mb-2 items-start";
   if (!agentChat) {
@@ -101,34 +131,33 @@ function attatchBotMessage(data) {
             <div class="flex items-end justify-center">
             <img src="./Assets/botImage.png" class="w-5 h-5 rounded-full mr-2 block" alt="Bot" />
             <div class="text-left pe-9">
-              <div class="p-2 bg-gray-100 w-fit text-left rounded-lg">
-                // ${botMessageText.message}
-                <p>message from bot</p>
+              <div class="p-2 bg-gray-100 w-fit text-left rounded-lg rounded-bl-none">
+                <p>${botMessageText.message}</p>
               </div>
+              <p class="text-white text-[10px] mt-[7.5px]">message from bot</p>
             </div>
             </div>
             </div>
           `;
     messages.appendChild(botMessage);
-  }
-  else {
+  } else {
     botMessage.innerHTML = `
   <div class="flex flex-col items-end justify-center">
     <div class="flex items-end justify-center">
     <img src="./Assets/botImage.png" class="w-5 h-5 rounded-full mr-2 block" alt="Bot" />
     <div class="text-left pe-9">
       <div class="p-2 bg-gray-100 w-fit text-left rounded-lg">
-        // ${botMessageText.message}
-        <p>message from bot</p>
+        <p>${botMessageText.message}</p>
       </div>
-      <div class="p-2 bg-gray-100 w-fit text-left rounded-lg mt-2">
+      <p class="text-white text-[10px] mt-[7.5px]">message from bot</p>
+      <div class="p-2 bg-gray-100 w-fit text-left rounded-lg rounded-bl-none mt-2">
         Do you want to chat with Customer Support?
       </div>
     </div>
     </div>
     <div class="block w-full ps-7 chatbot-helpful">
-        <button class="px-3 py-2 border border-[#0BB2C0] text-[#0BB2C0] w-fit text-left rounded-lg mt-2">Yes</button>
-        <button  class="px-3 py-2  border border-[#0BB2C0] text-[#0BB2C0] w-fit text-left rounded-lg mt-2"  onClick="handleNo()">No</button>
+        <button class="px-3 py-2 border border-[#0BB2C0] text-[#0BB2C0] w-fit text-left rounded-lg mt-2" onClick="handleYes()">Yes</button>
+        <button  class="px-3 py-2  border border-[#0BB2C0] text-[#0BB2C0] w-fit text-left rounded-lg mt-2">No</button>
       </div>
     </div>
   
@@ -137,7 +166,6 @@ function attatchBotMessage(data) {
   }
 }
 
-
 function sendMessage(event) {
   event.preventDefault();
   const input = event.target.elements.message;
@@ -145,17 +173,17 @@ function sendMessage(event) {
 
   if (checkMessage(newMessage)) {
     agentChat = true;
-  }
-  else {
+  } else {
     agentChat = false;
   }
 
-  wss.send(JSON.stringify({
-    "message": newMessage
-  }));
+  wss.send(
+    JSON.stringify({
+      message: newMessage,
+    })
+  );
   console.log("send message called");
   if (newMessage !== "") {
-
     const timestamp = new Date().toLocaleTimeString([], {
       hour: "numeric",
       minute: "numeric",
@@ -163,7 +191,7 @@ function sendMessage(event) {
 
     if (messages.childElementCount > 1) {
       messageContainer = document.getElementById("messageContainer");
-      messageContainer.classList.remove("flex")
+      messageContainer.classList.remove("flex");
     }
 
     const userMessage = document.createElement("div");
@@ -171,7 +199,7 @@ function sendMessage(event) {
     userMessage.innerHTML = `
   <div class="flex items-end justify-center">
     <img src="./Assets/botImage.png" class="w-5 h-5 rounded-full mr-2 hidden" alt="bot" />
-    <div class="p-2 bg-cyan-500 text-right text-white rounded-lg w-fit">
+    <div class="p-2 bg-cyan-500 text-right text-white rounded-lg  rounded-br-none w-fit">
       ${newMessage}
     </div>
   </div>
@@ -180,6 +208,7 @@ function sendMessage(event) {
   </div>
   `;
     messages.appendChild(userMessage);
+    addTyping();
 
     const helpfulElements = document.getElementsByClassName("chatbot-helpful");
 
@@ -187,16 +216,14 @@ function sendMessage(event) {
       helpfulElement.remove();
     });
 
-
     input.value = "";
     console.log(messages);
     setTimeout(scrollToBottom, 500);
   }
   // For Testing Keyword Response
-  attatchBotMessage({
-    "data": "{\"message\":\"Hello, I am the bot!\"}"
-  }
-  )
+  //   attatchBotMessage({
+  //     data: '{"message":"Hello, I am the bot!"}',
+  //   });
 }
 
 function scrollToBottom() {
@@ -205,8 +232,6 @@ function scrollToBottom() {
 }
 
 function toggleChatbot() {
-
-
   const chatbotContainer = document.getElementById("chat-bot__Container");
   chatbotContainer.classList.toggle("opacity-0");
   chatbotContainer.classList.toggle("opacity-100");
